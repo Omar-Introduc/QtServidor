aaa191679b23145e452fd1a21c54f6d6de44235a
Omar Ther <christian.hermoza.t@uni.pe>
2025-05-05 21:03:55 -0500

    Mensaje descriptivo del ca

--- servidor.cpp
+++ servidor.cpp
@@ -6,9 +6,47 @@ servidor::servidor(QWidget *parent)
     , ui(new Ui::servidor)
 {
     ui->setupUi(this);
+    connect(ui->pb_cerrar,&QAbstractButton::clicked,this,&QWidget::close);
+    //adiconar
+    connect(serverSocket,&QTcpServer::newConnection,this,&servidor::sendRandomMessage);
+
+    for(int i=)
 }
 
 servidor::~servidor()
 {
     delete ui;
+}
+
+void servidor::initServer()
+{
+    qDebug()<<"initServer!!!!";
+
+    if(!serverSocket->listen()){
+        qDebug()<<"Error en el servidor:"+ serverSocket->errorString();
+        return;
+    }
+    //Obtener parametros
+    QString port=QString::number(serverSocket->serverPort());
+
+    QString ipAdress;
+    QList<QHostAddress> ipAdressList=QNetworkInterface::allAddresses();
+
+    for(int i=0; i<ipAdressList.size();++i){
+        if(ipAdressList.at(i) !=QHostAddress::LocalHost && ipAdressList.at(i).toIPv4Address()){
+            ipAdress=ipAdressList.at(i).toString();
+            qDebug() << ipAdress;
+            break;
+        }
+    }
+}
+
+void servidor::sendRandomMessage()
+{
+    qDebug()<<"Send Random message)!!!";
+
+    //contenedor de dtos binarios y son las cadenas que se van a enviar
+    QByteArray qByteArray;
+    QDataStream qDataStream(&qByteArray,QIODevice)
+
 }
--- servidor.h
+++ servidor.h
@@ -2,6 +2,12 @@
 #define SERVIDOR_H
 
 #include <QWidget>
+#include <QTcpSocket>
+#include <QRandomGenerator64>
+#include <QTcpServer>
+ #include <QNetworkInterface>
+
+//para obtener las IP
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -19,5 +25,14 @@ public:
 
 private:
     Ui::servidor *ui;
+    QTcpServer*serverSocket=nullptr;
+
+    //donde se guardan los mensajes
+    QVector<QString> randomMessages;
+    void initServer();
+private slots:
+
+    void sendRandomMessage();
+
 };
 #endif // SERVIDOR_H
